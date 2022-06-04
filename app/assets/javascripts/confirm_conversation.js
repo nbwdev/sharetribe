8ea9539b43e10f5fc0d3717e5b10a4a6ff9d37b9
rwblyth <robblyth77@gmail.com>
2022-06-04 15:59:09 +0200

    Improve messaging on dispute and confirmation

--- app/assets/javascripts/confirm_conversation.js
+++ app/assets/javascripts/confirm_conversation.js
@@ -1,6 +1,10 @@
 window.ST = window.ST || {};
 (function(module) {
   var options;
+  var sellerMessagePlaceholder;
+  var disputeMessagePlaceholder;
+  var sellerMessageLabel;
+  var disputeMessageLabel;
 
   var onChangeCanceledFlow = function(selector) {
     switch(selector) {
@@ -16,6 +20,8 @@ window.ST = window.ST || {};
       $('.cancel-description').removeClass('hidden');
       $('.send-message-description').addClass('hidden');
       $('.send-message-form').removeClass('hidden');
+      $('#confirm-message-label').text(disputeMessageLabel);
+      $('#message_content').attr("placeholder", disputeMessagePlaceholder);
       $('.confirm-description, .close-listing-radio-buttons').addClass('hidden');
       break;
       case '.js-send-message-radio-button':
@@ -23,14 +29,20 @@ window.ST = window.ST || {};
         $('.send-message-description').removeClass('hidden');
         $('.cancel-description').addClass('hidden');
         $('.send-message-form').removeClass('hidden');
+        $('#confirm-message-label').text(sellerMessageLabel);
+        $('#message_content').attr("placeholder", sellerMessagePlaceholder);
         $('.confirm-description, .close-listing-radio-buttons').addClass('hidden');
       break;
     }
   };
 
   var init = function(initOptions) {
-    options = initOptions;
-    ST.initializeRadioButtons({
+    options = initOptions || {};
+    sellerMessagePlaceholder = options.sellerMessagePlaceholder;
+    disputeMessagePlaceholder = options.disputeMessagePlaceholder;
+    sellerMessageLabel = options.sellerMessageLabel;
+    disputeMessageLabel = options.disputeMessageLabel;
+      ST.initializeRadioButtons({
       buttons: ['.js-confirm-radio-button', '.js-cancel-radio-button', '.js-send-message-radio-button'],
       input: '.js-confirmation-status',
       callback: onChangeCanceledFlow
