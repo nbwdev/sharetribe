15bfd1e3206994aae146f090de9c5614e7dac993
rwblyth <robblyth77@gmail.com>
2022-12-21 20:41:20 +0100

    Fix the meta title and description for google and pinterest

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,31 +5,37 @@ class BlogController < ApplicationController
         'why_we_started_neverbeenworn' => {
             :id => 'why_we_started_neverbeenworn',
             :title => "Why we started NeverBeenWorn",
+            :description => "We want to help all you lovely ladies out there clear out your wardrobe and find a great new home for all your stylish unworn items",
             :created => Date.new(2022,7,5)
         },
         'sustainability_in_autumn' => {
             :id => 'sustainability_in_autumn',
             :title => 'Sustainability in autumn',
+            :description => "As NeverBeenWorn goes into its first autumn, we are taking a quick moment to reflect on how far we have come and looking forward to the next steps in our journey through more sustainable fashion",
             :created => Date.new(2022,10,9)
         },
         'big_boots_how_to_wear_them' => {
             :id => 'big_boots_how_to_wear_them',
             :title => 'Big Boots - How to Wear Them',
+            :description => "Big boots are here for another autumn and winter season. Here's how to choose the right pair for you",
             :created => Date.new(2022,11,15)
         },
         'the_wardrobe_challenge' => {
             :id => 'the_wardrobe_challenge',
             :title => 'The Wardrobe Challenge',
+            :description => "Take on your wardrobe, take back your time and banish that mess for good",
             :created => Date.new(2022,11,19)
         },
         'too_many_scarves' => {
             :id => 'too_many_scarves',
             :title => 'Too Many Scarves',
+            :description => "How I went from 18 scarves to just 4",
             :created => Date.new(2022,12,14)
         },
         'fix_your_drawers_forever' => {
             :id => 'fix_your_drawers_forever',
             :title => 'Fix your drawers forever',
+            :description => "From nightmare drawers to no more trouble",
             :created => Date.new(2022,12,16)
         }
     }
@@ -53,7 +59,9 @@ class BlogController < ApplicationController
         article_id = params[:id]
         render locals: {
             article_id: article_id,
-            article_data: ARTICLES[article_id]
+            article_data: ARTICLES[article_id],
+            social_title: ARTICLES[article_id][:title],
+            social_description: ARTICLES[article_id][:description]
         }
     end
 
